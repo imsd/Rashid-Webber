c7b65ee442aea994e689e64f57f4e7b89b229ec0
Exyiled <student@ent-v314-014.local>
2017-09-07 15:52:44 -0400

    Week2

---
+++ Unity Intro/Assets/Scripts/Rotating.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotating : MonoBehaviour {
+
+	// Use this for initialization
+	public Vector3 myRotation;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (myRotation * Time.deltaTime);
+	}
+}
---
+++ Unity Intro/Assets/Scripts/spawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner : MonoBehaviour {
+
+	// Use this for initialization
+
+	public GameObject spawnBox ;
+	public float startAfter = 1.0f;
+	public float spawnWhen = 0.5f;	
+
+	void Start () {
+	//	spawnBox = GameObject
+		InvokeRepeating("SpawnThing", startAfter , spawnWhen);
+		//first argument is the ofucntion u want to repeat
+		// after how many seconds do you want it start
+		// how many seconds of repetition
+	}
+	
+	// Update is called once per frame
+	void SpawnThing () {
+		transform.position = new Vector3 (Random.Range(-5f,5f), 3, Random.Range(-5f,5f));
+		// first atgument is the object itse;f
+		//second argument is the position
+		// third is the rotation. 
+		//Quaternion.identity = default rotation
+
+
+		Instantiate(spawnBox,transform.position,Quaternion.identity);
+		Debug.Log (transform.position);
+	}
+}
