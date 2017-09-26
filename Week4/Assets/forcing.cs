e7f71b15fa2316a69a978fdfda36b09937a54ce0
Exyiled <student@ent-v314-014.local>
2017-09-26 15:40:15 -0400

    Commit

---
+++ Week4/Assets/forcing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class forcing : MonoBehaviour {
+
+	public float forcePower = 1000f;
+
+	// function gets called when the mouse is hovering
+	// over the object
+	void OnMouseDown() {
+		Vector3 forceDirection = Vector3.up * forcePower;
+		GetComponent<Rigidbody> ().AddForce (forceDirection);
+	}
+
+	void onTriggerEnter(Collider coll){
+		if (coll.gameObject.tag == "floor") {
+			Debug.Log ("Touch");
+		}
+	}
+}
