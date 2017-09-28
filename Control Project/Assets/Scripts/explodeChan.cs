5bf4f2ed7194daf51e73fdd227ccf56df43e1ad6
Exyiled <student@ent-v314-014.local>
2017-09-28 15:54:18 -0400

    Todays update

---
+++ Control Project/Assets/Scripts/explodeChan.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explodeChan : MonoBehaviour {
+
+
+	public float thrust;
+	public Rigidbody rb;
+	public float timer;
+
+
+	// keep track of whether or not the object is active
+	bool isActive = false;
+
+	// the renderer for this object
+	public Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		// at start, get the Renderer component of this object
+		rend = GetComponent<Renderer> ();
+		rb = GetComponent<Rigidbody>();
+		timer = 3;
+	}
+
+	void Update () {
+		if (isActive) {
+			// shake randomly within .1 unit
+			Vector3 randomShake = Random.insideUnitSphere * .7f;
+			transform.position += randomShake;
+			//Launch
+			timer -= Time.deltaTime;
+			if(timer < 0){
+				rb.AddForce(transform.up * thrust);
+				timer = 3f;
+				isActive = !isActive;
+			}
+		}
+	}
+
+	void OnMouseDown() {
+		// invert the value
+		isActive = !isActive;
+
+		// set color based on change
+		if (isActive) {
+			rend.material.color = Color.red;
+		} else {
+			rend.material.color = Color.white;
+		}
+	}
+}
