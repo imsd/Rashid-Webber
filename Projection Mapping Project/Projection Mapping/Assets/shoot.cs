e0553b9f262e5f7398fb4c5bf4f0e4668a68b44c
Exyiled <31451975+Exyiled@users.noreply.github.com>
2017-10-26 12:16:37 -0400

    Project 2
    Projection Mapping

---
+++ Projection Mapping Project/Projection Mapping/Assets/shoot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoot : MonoBehaviour {
+
+
+	public GameObject ball;
+	public float power = 2000f;
+	public float xDir ;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject go = Instantiate (ball, transform.position, Quaternion.identity) as GameObject;
+			//	go.transform.LookAt(position);    
+			go.GetComponent<Rigidbody> ().AddForce(xDir, 0, -1 * power, ForceMode.Impulse);;
+		}	
+	}
+}
