828503d3f0cb616e5516edd5a301e759d643ed56
Exyiled <31451975+Exyiled@users.noreply.github.com>
2017-10-30 22:58:34 -0400

    Update

---
+++ Projection Mapping Project/Projection Mapping Final/Assets/shootBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shootBall : MonoBehaviour {
+
+
+	public GameObject ball;
+	public float power = 2000f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+		GameObject go = Instantiate(ball, transform.position, Quaternion.identity) as GameObject;    
+			go.GetComponent<Rigidbody>().AddForce(go.transform.up * power);
+		}
+	}
+}
