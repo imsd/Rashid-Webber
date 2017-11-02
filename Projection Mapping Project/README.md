8cea071ff64be2d5c920feff67e016e4b7802bb5
Exyiled <student@ent-v314-014.local>
2017-11-02 15:28:31 -0400

    Week 10 Day 2

--- Projection Mapping Project/README.md
+++ Projection Mapping Project/README.md
@@ -1,8 +1,12 @@
-# Projection Mapping Project
+# Projection Mapping 
 
 ## Rashid , Ryan Y, Ryan C, Catherine , Mike 
 
 ## Summary
 
-We wanted to create a unity project where we played with the spaces of the cube. The balls would spawn in one box and pushed to the next and then bounced into another. By hitting Space you would spawn more balls
+We wanted to create a unity project where we played with the spaces of the cube. The balls would spawn in one box and pushed to the next and then bounced into another. 
+
+## Controls
+
+By hitting Space you would spawn more balls
 
---
+++ Week 10/Assets/arrayStringExanmpe.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class arrayStringExanmpe : MonoBehaviour {
+
+
+	public string [] names;
+	public string [] places;
+	public string [] places;
+	public string [] places;
+	public string [] places;
+	public string [] places;
+	public string [] places;
+	public string [] places;
+	public string [] places;
+	public string [] places;
+
+	// public string [] Not array;
+
+	// Use this for initialization
+
+	void Start () {
+//		for(int i = 0; i < names.Length; i++){
+//		Debug.Log (names[1]);
+//	}
+
+//		Debug.Log (Random.Range (0f, 3f));
+
+		int randNoun = Random.Range (0, names.Length);
+		int randPlaces = Random.Range (0, places.Length);
+//		Debug.Log(names [rando]);
+
+		string line1 = "One day, " + names [randNoun] + " went to " + places[randPlaces] + ".";
+		Debug.Log (line1);
+
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
