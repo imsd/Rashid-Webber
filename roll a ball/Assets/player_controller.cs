ae83e766d231471a47c1c783580abf4a9d95a6f8
Exyiled <student@ent-v314-014.local>
2017-11-14 15:43:21 -0500

    update

---
+++ roll a ball/Assets/player_controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player_controller : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public float myVariable;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody> ();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float MoveHorizontal = Input.GetAxis ("Horizontal");
+		float MoveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (MoveHorizontal, 0.0f, MoveVertical);
+
+		rb.AddForce(movement * speed);
+	}
+}
